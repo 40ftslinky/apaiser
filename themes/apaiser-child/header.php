f0a387f2eef7117cfcbafbdf65c4038490aeb49d
40ftslinky <jez@40ftslinky.com>
2024-11-10 23:01:58 +1100

    font rendering solution - independent src

--- themes/apaiser-child/header.php
+++ themes/apaiser-child/header.php
@@ -17,6 +17,7 @@
 
 	?>
     <meta charset="UTF-8">
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"> 
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $page_title ?> | Apaiser</title>
 
